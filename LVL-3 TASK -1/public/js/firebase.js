ca74eb26716e91d93fd1ae37bbf01892256ed4fb
NAWAZ-SRM <115914519+NAWAZ-SRM@users.noreply.github.com>
2024-01-26 19:12:38 +0530

    Add files via upload

---
+++ LVL-3 TASK -1/public/js/firebase.js
@@ -0,0 +1,12 @@
+// let firebaseConfig = {
+//     apiKey: "AIzaSyD7rgVnNN-d9qGowiPUnD9cmACTGJ5vchM",
+//   authDomain: "blog-codsoft.firebaseapp.com",
+//   projectId: "blog-codsoft",
+//   storageBucket: "blog-codsoft.appspot.com",
+//   messagingSenderId: "711037161297",
+//   appId: "1:711037161297:web:1b08aee3b46ea3dde8cd70"
+// };
+
+// firebase.initializeApp(firebaseConfig);
+
+// let db = firebase.firestore();
